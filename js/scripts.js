57b8f4007e3ea843d8d19794e30f3f74948cb2a0
abdullahalwafi <abdullahalwafi16@gmail.com>
2025-12-26 15:24:47 +0700

    Feat: flash sale product

--- js/scripts.js
+++ js/scripts.js
@@ -22,3 +22,56 @@ document.querySelectorAll(".mega-left .nav-link").forEach((link) => {
     }
   });
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const track = document.getElementById("flashSaleTrack");
+  const items = document.querySelectorAll(".flash-item");
+  const visible = 4;
+  let index = 0;
+
+  // CLONE AWAL & AKHIR (BIAR LOOP)
+  for (let i = 0; i < visible; i++) {
+    track.appendChild(items[i].cloneNode(true));
+    track.insertBefore(
+      items[items.length - 1 - i].cloneNode(true),
+      track.firstChild
+    );
+  }
+
+  const total = track.children.length;
+  index = visible;
+
+  track.style.transform = `translateX(-${index * 25}%)`;
+
+  function slideNext() {
+    index++;
+    track.style.transition = "transform .5s ease";
+    track.style.transform = `translateX(-${index * 25}%)`;
+
+    if (index >= total - visible) {
+      setTimeout(() => {
+        track.style.transition = "none";
+        index = visible;
+        track.style.transform = `translateX(-${index * 25}%)`;
+      }, 500);
+    }
+  }
+
+  function slidePrev() {
+    index--;
+    track.style.transition = "transform .5s ease";
+    track.style.transform = `translateX(-${index * 25}%)`;
+
+    if (index <= 0) {
+      setTimeout(() => {
+        track.style.transition = "none";
+        index = total - visible * 2;
+        track.style.transform = `translateX(-${index * 25}%)`;
+      }, 500);
+    }
+  }
+
+  document.querySelector(".flash-next").onclick = slideNext;
+  document.querySelector(".flash-prev").onclick = slidePrev;
+
+  setInterval(slideNext, 3000);
+});
